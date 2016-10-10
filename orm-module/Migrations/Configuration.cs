c556ce2b15149e96075f1e534d7171ebad6120cf
Pavel Dziashchenia <pavel_dziashchenia@epam.com>
2016-10-10 20:22:50 +0300

    p3 done

--- orm-module/Migrations/Configuration.cs
+++ orm-module/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using orm_module.Entities;
+
 namespace orm_module.Migrations
 {
     using System;
@@ -12,7 +14,7 @@ namespace orm_module.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(orm_module.Entities.Northwind context)
+        protected override void Seed(Northwind context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -26,6 +28,24 @@ namespace orm_module.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Categories.AddOrUpdate(
+                c => c.CategoryName,
+                new Category() { CategoryName = "Cat1" },
+                new Category() { CategoryName = "Cat2" },
+                new Category() { CategoryName = "Cat3" });
+
+            context.Regions.AddOrUpdate(
+                r => r.RegionID,
+                new Region() { RegionID = 1, RegionDescription = "RegDescr1" },
+                new Region() { RegionID = 2, RegionDescription = "RegDescr2" },
+                new Region() { RegionID = 3, RegionDescription = "RegDescr3" });
+
+            context.Territories.AddOrUpdate(
+                t => t.TerritoryID,
+                new Territory() { TerritoryID = "terid1", TerritoryDescription = "descr1", RegionID = 1 },
+                new Territory() { TerritoryID = "terid2", TerritoryDescription = "descr2", RegionID = 1 },
+                new Territory() { TerritoryID = "terid3", TerritoryDescription = "descr5", RegionID = 3 });
         }
     }
 }
